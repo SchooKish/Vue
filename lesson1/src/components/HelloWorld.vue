a35dfde5276656bb0e856cd6788a4544a466d740
SchooKish <kishkola@gmail.com>
2021-04-28 22:07:32 +0300

    add hw1

---
+++ lesson1/src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <input v-model.number="operand1" />
+    <input v-model.number="operand2" /> = {{ result }}
+    <div>
+      <button @click="calc(1)">+</button>
+      <button @click="calc(2)">-</button>
+      <button @click="calc(3)">*</button>
+      <button @click="calc(4)">/</button>
+      <button @click="calc(5)">^</button>
+      <button @click="calc(6)">%</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: () =>({
+    operand1: 0,
+    operand2: 0,
+    result: 0
+  }),
+  props: {
+  },
+  methods: {
+    calc(op) {
+      let value = op;
+
+      switch(value) {
+        case 1:
+          this.result = this.operand1 + this.operand2;
+          break;
+        case 2:
+          this.result = this.operand1 - this.operand2;
+          break;
+        case 3:
+          this.result = this.operand1 * this.operand2;
+          break;
+        case 4:
+          this.result = this.operand1 / this.operand2;
+          break;
+        case 5:
+          this.result = this.operand1 ** this.operand2;
+          break;
+        case 6:
+          this.result = this.operand1 % this.operand2;
+          break;
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- <style scoped lang="scss">
+</style> -->
