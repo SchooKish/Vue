fcda02dd1b354d1617a321c3a262c4c0933e1c8a
SchooKish <kishkola@gmail.com>
2021-05-20 07:45:10 +0300

    add hw 3

---
+++ lesson3/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <header>
+      
+    </header>
+    <main>
+      <button
+      type="button"
+      class="btn"
+      @click="showModal"
+    >
+      Add new cost
+    </button>
+      <Form  v-show="isModalVisible"
+      @close="closeModal" @add="add"/>
+      <Main :items="mainList" />
+    </main>
+  </div>
+</template>
+
+<script>
+import Main from "./components/Main";
+import Form from "./components/Form";
+
+export default {
+  name: "App",
+  components: {
+    Main,
+    Form,
+  },
+  data() {
+    return {
+      isModalVisible: false,
+
+      mainList: []
+    }
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
+    add(date) {
+      this.mainList.push(date)
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
+</style>
